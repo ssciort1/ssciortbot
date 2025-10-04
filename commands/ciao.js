1b37034ef956b244fe3c63fb0e1033139db613de
Salvo Sciortino <ssciort@gmail.com>
2025-10-04 13:36:51 +0200

    Messaggio chiaro sulle modifiche

--- commands/ciao.js
+++ commands/ciao.js
@@ -2,6 +2,9 @@ module.exports = {
     name: 'ciao',
     description: 'Saluta il bot',
     async execute(msg) {
-        msg.reply('Ciao io sono SsciortBot');
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        msg.reply(`Ciao io sono SsciortBot, sono attualmente ${hours}:${minutes}`);
     }
 };
